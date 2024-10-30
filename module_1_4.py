59e199057bb92334f09db491ac4903510f1a6488
UstasXXX <sirius20011990@gmail.com>
2024-10-30 17:52:56 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,16 +1,17 @@
 my_string = input('Здравствуй онлайн институт ')
 print (len(my_string))
 
-my_string = 'Здравствуй онлайн институт '
+my_string = 'Здравствуй онлайн институт'
 print (my_string.upper())
 
-my_string = 'Здравствуй онлайн институт '
+my_string = 'Здравствуй онлайн институт'
 print (my_string.lower())
 
-my_string = 'Здравствуй онлайн институт '
+my_string = 'Здравствуй онлайн институт'
 print (my_string.replace(' ',''))
 
-my_string = 'Здравствуй онлайн институт '
+my_string = 'Здравствуй онлайн институт'
 print (my_string[0])
 
-my_string = 'Здравствуй онлайн институт '
+my_string = 'Здравствуй онлайн институт'
+print(my_string[-1])
